56b393890fbbb55e9bbc93ce5feae6a934aa493c
Henry X <henryxia9999@gmail.com>
2021-12-14 23:20:09 -0600

    Presently my soul grew stronger, hesitating then no longer, "Sir," said I, "or Madam, truly your forgiveness I implore. But the fact is I was napping, and so gently you came rapping, and so faintly you came tapping, tapping at my chamber door, that I scarce was sure I heard you." Here I opened wide the door, Darkness there and nothing more.

---
+++ advent-of-cold/2021/15.cpp
@@ -0,0 +1,3 @@
+// rm'd the file by accident... rip
+// locked out for 5 mins for dividing by 5 instead of n in multiple places
+// sad
--- advent-of-cold/sandbox/t.py
+++ advent-of-cold/sandbox/t.py
@@ -1,9 +1,11 @@
 from collections import deque, defaultdict
 from itertools import product, permutations, accumulate
 from itertools import zip_longest, groupby, takewhile
+from heapq import heappush, heappop, heapify, merge
 import sys
 big = 1999999999999999999
 IN = [ x.strip() for x in sys.stdin ]
+
 
 
 
